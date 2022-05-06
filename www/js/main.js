5870fc3acbfcc856c672bdf1f40560d0505c62c8
Jim Carr <jfcarr@gmail.com>
2022-05-05 21:06:11 -0400

    1) Moved JavaScript to a standalone file, and 2) reorganized coyote audio files.

---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+function adjustCoyoteVolume(direction) {
+	currentVolume = document.getElementById('player').volume;
+
+	currentVolume = currentVolume + (direction == 'up' ? 0.1 : -0.1);
+
+	if (currentVolume > 1) { currentVolume = 1; }
+	if (currentVolume < 0) { currentVolume = 0; }
+
+	document.getElementById('player').volume = currentVolume;
+	document.getElementById('volumebadge').innerText = 'Volume: ' + Math.round(currentVolume * 100) + '%';
+}
+
+function updateCoyoteAudioSrc() {
+	var selected = 'media/coyote/' + document.getElementById('coyote_audio_src').value;
+
+	document.getElementById("player").setAttribute('src', selected);
+}
+
+function setLoopState() {
+	document.getElementById("player").loop = (document.getElementById("loopAudio").checked ? true : false);
+}
