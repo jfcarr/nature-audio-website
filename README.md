d6b4e8f681f61252a6059dd4bbfec2f90deac126
Jim Carr <jfcarr@gmail.com>
2022-12-03 22:37:43 -0500

    Added Great Horned Owl and Eastern Screech Owl.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Nature Audio website
 
-This is a simple website providing a selection of nature sounds (coyote only, for now).  It's responsive, so it also works well on a mobile browser.
+This is a simple website providing a selection of nature sounds (Coyote, Eastern Screech Owl, and Great Horned Owl, for now).  It's responsive, so it also works well on a mobile browser.
 
 The current version is published at https://natureaudio.goodguyscience.com/
--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,4 @@
-function adjustCoyoteVolume(direction) {
+function adjustVolume(direction) {
 	currentVolume = document.getElementById('player').volume;
 
 	currentVolume = currentVolume + (direction == 'up' ? 0.1 : -0.1);
@@ -10,8 +10,8 @@ function adjustCoyoteVolume(direction) {
 	document.getElementById('volumebadge').innerText = 'Volume: ' + Math.round(currentVolume * 100) + '%';
 }
 
-function updateCoyoteAudioSrc() {
-	var selected = 'media/coyote/' + document.getElementById('coyote_audio_src').value;
+function updateAudioSrc() {
+	var selected = 'media/' + document.getElementById('audio_src').value;
 
 	document.getElementById("player").setAttribute('src', selected);
 }
